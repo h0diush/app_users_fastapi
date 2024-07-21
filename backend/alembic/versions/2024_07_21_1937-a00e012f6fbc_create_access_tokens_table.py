3fe4d38cd905cf5cc102643657a3dc0b41c3558e
h0diush <h0diush@yandex.ru>
2024-07-21 19:38:41 +0300

    add table access_tokens

---
+++ backend/alembic/versions/2024_07_21_1937-a00e012f6fbc_create_access_tokens_table.py
@@ -0,0 +1,51 @@
+"""create access_tokens table
+
+Revision ID: a00e012f6fbc
+Revises: be3ccdc76003
+Create Date: 2024-07-21 19:37:47.237093
+
+"""
+
+from typing import Sequence, Union
+
+import fastapi_users_db_sqlalchemy
+from alembic import op
+import sqlalchemy as sa
+
+
+revision: str = "a00e012f6fbc"
+down_revision: Union[str, None] = "be3ccdc76003"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        "access_tokens",
+        sa.Column("user_id", sa.Integer(), nullable=False),
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("token", sa.String(length=43), nullable=False),
+        sa.Column(
+            "created_at",
+            fastapi_users_db_sqlalchemy.generics.TIMESTAMPAware(timezone=True),
+            nullable=False,
+        ),
+        sa.ForeignKeyConstraint(
+            ["user_id"],
+            ["users.id"],
+            name=op.f("fk_access_tokens_user_id_users"),
+            ondelete="cascade",
+        ),
+        sa.PrimaryKeyConstraint("id", "token", name=op.f("pk_access_tokens")),
+    )
+    op.create_index(
+        op.f("ix_access_tokens_created_at"),
+        "access_tokens",
+        ["created_at"],
+        unique=False,
+    )
+
+
+def downgrade() -> None:
+    op.drop_index(op.f("ix_access_tokens_created_at"), table_name="access_tokens")
+    op.drop_table("access_tokens")
