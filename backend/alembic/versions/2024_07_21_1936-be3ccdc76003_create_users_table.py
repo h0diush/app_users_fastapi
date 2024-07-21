87ec4684188e26557cedb164c1c793fd5cd17482
h0diush <h0diush@yandex.ru>
2024-07-21 19:36:57 +0300

    add new table users

--- backend/alembic/versions/2024_07_21_1936-be3ccdc76003_create_users_table.py
+++ backend/alembic/versions/2024_07_21_1936-be3ccdc76003_create_users_table.py
@@ -1,8 +1,8 @@
 """create users table
 
-Revision ID: e408069d7397
+Revision ID: be3ccdc76003
 Revises: 
-Create Date: 2024-07-21 18:34:52.943009
+Create Date: 2024-07-21 19:36:03.716748
 
 """
 
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = "e408069d7397"
+revision: str = "be3ccdc76003"
 down_revision: Union[str, None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
@@ -21,20 +21,18 @@ depends_on: Union[str, Sequence[str], None] = None
 
 def upgrade() -> None:
     op.create_table(
-        "user",
+        "users",
         sa.Column("id", sa.Integer(), nullable=False),
         sa.Column("email", sa.String(length=320), nullable=False),
         sa.Column("hashed_password", sa.String(length=1024), nullable=False),
         sa.Column("is_active", sa.Boolean(), nullable=False),
         sa.Column("is_superuser", sa.Boolean(), nullable=False),
         sa.Column("is_verified", sa.Boolean(), nullable=False),
-        sa.PrimaryKeyConstraint("id", name=op.f("pk_user")),
+        sa.PrimaryKeyConstraint("id", name=op.f("pk_users")),
     )
-    op.create_index(op.f("ix_user_email"), "user", ["email"], unique=True)
-    # ### end Alembic commands ###
+    op.create_index(op.f("ix_users_email"), "users", ["email"], unique=True)
 
 
 def downgrade() -> None:
-    op.drop_index(op.f("ix_user_email"), table_name="user")
-    op.drop_table("user")
-    # ### end Alembic commands ###
+    op.drop_index(op.f("ix_users_email"), table_name="users")
+    op.drop_table("users")
