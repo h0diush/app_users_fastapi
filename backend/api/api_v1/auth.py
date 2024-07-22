35a52a931a211dd74b402ad064f4ff417aedf198
h0diush <h0diush@yandex.ru>
2024-07-22 06:09:04 +0300

    add auth router login and logaut

---
+++ backend/api/api_v1/auth.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter
+
+from api.dependencies.authentication.backend import authentication_backend
+from core.config import settings
+from .fastapi_users import fastapi_users
+
+router = APIRouter(prefix=settings.api.v1.prefix, tags=["Auth"])
+
+# /login
+# / logout
+router.include_router(
+    fastapi_users.get_auth_router(
+        authentication_backend,
+    )
+)
