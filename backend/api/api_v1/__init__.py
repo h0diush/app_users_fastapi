1c1fa06ab73bcab297787e43fd2cf42178c7b734
h0diush <h0diush@yandex.ru>
2024-07-23 22:46:04 +0300

    add http bearer

--- backend/api/api_v1/__init__.py
+++ backend/api/api_v1/__init__.py
@@ -1,9 +1,14 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from fastapi.security import HTTPBearer
 
 from core.config import settings
 from .auth import router as auth_router
 from .user import router as user_router
 
-router = APIRouter(prefix=settings.api.v1.prefix)
+http_bearer = HTTPBearer(auto_error=False)
+router = APIRouter(
+    prefix=settings.api.v1.prefix,
+    dependencies=[Depends(http_bearer)],
+)
 router.include_router(auth_router)
 router.include_router(user_router)
