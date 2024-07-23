377d570468c71e7f7d618e988ba3b2c9eb70cc49
h0diush <h0diush@yandex.ru>
2024-07-23 22:38:37 +0300

    add new user router and refactoring auth router

--- backend/api/api_v1/auth.py
+++ backend/api/api_v1/auth.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 
 from api.dependencies.authentication.backend import authentication_backend
 from core.config import settings
+from core.schemas.user import UserRead, UserCreate
 from .fastapi_users import fastapi_users
 
 router = APIRouter(prefix=settings.api.v1.auth, tags=["Auth"])
@@ -11,5 +12,14 @@ router = APIRouter(prefix=settings.api.v1.auth, tags=["Auth"])
 router.include_router(
     fastapi_users.get_auth_router(
         authentication_backend,
+    )
+)
+
+
+# /register
+router.include_router(
+    fastapi_users.get_register_router(
+        UserRead,
+        UserCreate,
     )
 )
--- backend/api/api_v1/user.py
+++ backend/api/api_v1/user.py
@@ -1,8 +1,15 @@
 from fastapi import APIRouter
 
 from core.config import settings
-from core.schemas.user import UserRead, UserCreate
+from core.schemas.user import UserRead, UserUpdate
 from .fastapi_users import fastapi_users
 
-router = APIRouter(prefix=settings.api.v1.users)
-router.include_router(fastapi_users.get_register_router(UserRead, UserCreate))
+router = APIRouter(prefix=settings.api.v1.users, tags=["Users"])
+
+# /register
+router.include_router(
+    fastapi_users.get_users_router(
+        UserRead,
+        UserUpdate,
+    )
+)
